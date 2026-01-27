75a6ba4a108bed6aa563270a9d76a804eb4bf006
ArthurViniDev <kenzygamedev@gmail.com>
2026-01-27 19:24:28 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaDeTarefas/Models/TarefaModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SistemaDeTarefas.Models
+{
+    public class TarefaModel
+    {
+        public string? Descricao { get; set; }
+        public string? Nome { get; set; }
+        public int Status { get; set; }
+        public int Id { get; set; }
+    }
+}
---
+++ SistemaDeTarefas/Repositorios/Interfaces/IUsuarioRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Repositorios.Interfaces
+{
+    public interface IUsuarioRepositorio
+    {
+
+        Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id);
+        Task<UsuarioModel> Adicionar(UsuarioModel usuario);
+        Task<UsuarioModel> BuscarPorId(int id);
+
+        Task<List<UsuarioModel>> BuscarTodosUsuarios();
+        Task<bool> Deletar(int id);
+    }
+}
